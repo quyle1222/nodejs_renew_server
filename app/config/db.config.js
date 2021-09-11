e170a81e24c742446e6051354fe23b6c5ca4e0ca
tupt <tupt@dn.paracelsoft.com>
2021-09-11 16:58:41 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   HOST: "db.zerjhlvqkyjzhlnqmrgh.supabase.co",
-  PORT: 5432,
+  PORT: 543233,
   DB: "mongodb+srv://tupham231020:onljnekc123@project.zcahx.mongodb.net/test",
-  USER: "Onljnekc123",
 };
