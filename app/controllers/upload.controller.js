8250b9ca6d650a1d67c8f526193dcde579da9da0
tupt <tupt@dn.paracelsoft.com>
2021-09-13 07:12:42 +0700

    update

--- app/controllers/upload.controller.js
+++ app/controllers/upload.controller.js
@@ -2,12 +2,13 @@ const Resize = require("./../root/Resize");
 const path = require("path");
 
 const uploadFile = async (req, res) => {
-  const imagePath = path.join(__dirname, "/public/images");
+  const imagePath = path.join(__dirname, "../../public/images");
   const fileUpload = new Resize(imagePath);
 
   if (!req.file) {
     res.status(401).json({ error: "Please provide an image" });
   }
+  console.log(req.file);
   const filename = await fileUpload.save(req.file.buffer);
   return res.status(200).json({ name: filename });
 };
