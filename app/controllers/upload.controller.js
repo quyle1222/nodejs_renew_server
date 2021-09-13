2a74f1e373849bef396f1661ee6d82ad24967da7
tupt <tupt@dn.paracelsoft.com>
2021-09-13 08:31:34 +0700

    update

--- app/controllers/upload.controller.js
+++ app/controllers/upload.controller.js
@@ -4,27 +4,6 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 const config = require("../config/cloudinary.config");
 
-const uploadFile = async (req, res, next) => {
-  let streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-      let stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      });
-
-      streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-  };
-  async function upload(req) {
-    let result = await streamUpload(req);
-    console.log(result);
-  }
-  upload(req);
-};
-
 const uploadImage = (req, res, next) => {
   let streamUpload = (req) => {
     return new Promise((resolve, reject) => {
@@ -41,17 +20,26 @@ const uploadImage = (req, res, next) => {
     });
   };
 
-  async function upload(req) {
+  const upload = async (req) => {
     let result = await streamUpload(req);
     if (result) {
-      return res.status(200).j;
+      result.api_key = null;
+      return res.status(200).send({
+        message: "Upload success",
+        success: true,
+        data: result,
+      });
+    } else {
+      return res.status(500).send({
+        message: "Upload fail",
+        success: false,
+      });
     }
-  }
+  };
 
   upload(req);
 };
 
 module.exports = {
-  uploadFile,
   uploadImage,
 };
