32a5285b096307064bb89a6029afdc2b58680073
tupt <tupt@dn.paracelsoft.com>
2021-10-07 21:11:52 +0700

    update

--- app/routers/shipper.routes.js
+++ app/routers/shipper.routes.js
@@ -1,8 +1,8 @@
 const { verifySignUp, authJwt } = require("../middlewares");
 const controller = require("../controllers/shipper.controller");
 
-module.exports = function (app) {
-  app.use(function (req, res, next) {
+module.exports = function(app) {
+  app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "authorization, Origin, Content-Type, Accept",
@@ -16,20 +16,20 @@ module.exports = function (app) {
     controller.signUp,
   );
   app.post("/api/v1/shipper/signIn", controller.signIn);
-  app.get("/api/v1/shipper/getInfo", authJwt.verifyToken, controller.getInfo);
+  app.get("/api/v1/shipper/getInfo", [authJwt.verifyToken], controller.getInfo);
   app.get(
     "/api/v1/shipper/listOrderHistory",
-    authJwt.verifyToken,
+    [authJwt.verifyToken],
     controller.getInfo,
   );
   app.get(
     "/api/v1/shipper/getStatistical",
-    authJwt.verifyToken,
+    [authJwt.verifyToken],
     controller.getStatistical,
   );
   app.post(
     "/api/v1/shipper/approveOrder",
-    authJwt.verifyToken,
+    [authJwt.verifyToken],
     controller.approvedOrder,
   );
 };
